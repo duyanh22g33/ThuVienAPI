a026219f356935247b0d52fd380e9f90ff3925dd
DuyAnh <duyvo07t@gmail.com>
2026-09-23 16:30:18 +0700

    Add project files.

---
+++ WEBAPI/Models/Domain/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models.Domain
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public List<Book_Author> Book_Authors { get; set; }
+    }
+}
---
+++ WEBAPI/Models/Domain/Book_Author.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models.Domain
+{
+    public class Book_Author
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
